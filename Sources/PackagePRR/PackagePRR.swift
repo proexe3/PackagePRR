c7d4edd6980cdb87f02b02a78e4593d7caf3a526
Dhaval K <dhavalpkakadiya@gmail.com>
2024-04-01 15:02:13 +0530

    some changes

--- Sources/PackagePRR/PackagePRR.swift
+++ Sources/PackagePRR/PackagePRR.swift
@@ -1,11 +1,28 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import SwiftUI
 
-open class names {
-    public var employeeName: String
+
+//open class names {
+//    public var employeeName: String
+//    
+//    public init(empName: String) {
+//        self.employeeName = empName
+//    }
+//}
+
+@available(iOS 13.0.0, *)
+public struct MyScreen: View {
+    public init() {}
     
-    public init(empName: String) {
-        self.employeeName = empName
+    public var body: some View {
+        Text("Hello from My Screen!")
     }
+}
+
+// PublicInterface.swift
+@available(iOS 13.0.0, *)
+public func getMyScreen() -> MyScreen {
+    return MyScreen()
 }
